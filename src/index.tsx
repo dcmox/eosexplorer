7385aed176c0e7a6b70b72dbd9e8978275f86143
Daniel Moxon <dancmox@comcast.net>
2020-01-19 19:14:02 -0600

    remove test

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,6 @@ import App from './App'
 import './index.scss'
 import * as serviceWorker from './serviceWorker'
 
-import * as tests from './utils/csvToJson'
-import * as testData from './utils/test-data/csv'
-
-// import { DebounceAndRetry } from './utils/debounceAndRetry'
-
-// console.log('hmmm')
-
-// const debounce = new DebounceAndRetry(() => {
-//     console.log('this is a test')
-// })
-
-// debounce.start()
-
-console.log(tests.csvToJson(testData.csvData))
-
 // If we had custom routes we could add them here and build in better state management
 ReactDOM.render(<App />, document.getElementById('root'))
 
